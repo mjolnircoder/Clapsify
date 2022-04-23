aabde05632f61f35ca8ed49c6be8a91b66e4fd2c
Aditya <67043741+mjolnircoder@users.noreply.github.com>
2022-04-23 10:59:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,117 @@
+console.log("Welcome to Clapsify");
+
+//Initialize the variables
+let songIndex = 0;
+let audioElement = new Audio('songs/1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+
+
+let songs = [
+  {
+    songName: "Butter", filepath: "songs/1.mp3", coverPath: "cover/butter.png",
+  },
+  { songName: "Dynamite", filepath: "songs/2.mp3", coverPath: "cover/2.jpg" },
+  {
+    songName: "Boy With Luv", filepath: "songs/3.mp3", coverPath: "cover/3.jpg",
+  },
+  { songName: "Mic Drop", filepath: "songs/4.mp3", coverPath: "cover/4.jpg" },
+  {
+    songName: "My Universe",filepath: "songs/5.mp3", coverPath: "cover/5.jpg",
+  },
+  { songName: "Fake Love", filepath: "songs/6.mp3", coverPath: "cover/6.jpg" },
+  { songName: "Euphoria", filepath: "songs/7.mp3", coverPath: "cover/7.jpg" },
+  {
+    songName: "Permission to Dance", filepath: "songs/8.mp3", coverPath: "cover/8.jpg",
+  },
+];
+
+songItems.forEach((element, i)=>{ 
+  element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
+  element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
+})
+
+
+// Handle play/pause click
+masterPlay.addEventListener('click', ()=>{
+  if(audioElement.paused || audioElement.currentTime<=0){
+      audioElement.play();
+      masterPlay.classList.remove('fa-play-circle');
+      masterPlay.classList.add('fa-pause-circle');
+      gif.style.opacity = 1;
+  }
+  else{
+      audioElement.pause();
+      masterPlay.classList.remove('fa-pause-circle');
+      masterPlay.classList.add('fa-play-circle');
+      gif.style.opacity = 0;
+  }
+})
+// Listen to Events
+audioElement.addEventListener('timeupdate', ()=>{ 
+  // Update Seekbar
+  progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+  myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change', ()=>{
+  audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+const makeAllPlays = ()=>{
+  Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+      element.classList.remove('fa-pause-circle');
+      element.classList.add('fa-play-circle');
+  })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+  element.addEventListener('click', (e)=>{ 
+      makeAllPlays();
+      songIndex = parseInt(e.target.id);
+      e.target.classList.remove('fa-play-circle');
+      e.target.classList.add('fa-pause-circle');
+      audioElement.src = `songs/${songIndex+1}.mp3`;
+      masterSongName.innerText = songs[songIndex].songName;
+      audioElement.currentTime = 0;
+      audioElement.play();
+      gif.style.opacity = 1;
+      masterPlay.classList.remove('fa-play-circle');
+      masterPlay.classList.add('fa-pause-circle');
+  })
+})
+
+document.getElementById('next').addEventListener('click', ()=>{
+  if(songIndex>=7){
+      songIndex = 0
+  }
+  else{
+      songIndex += 1;
+  }
+  audioElement.src = `songs/${songIndex+1}.mp3`;
+  masterSongName.innerText = songs[songIndex].songName;
+  audioElement.currentTime = 0;
+  audioElement.play();
+  masterPlay.classList.remove('fa-play-circle');
+  masterPlay.classList.add('fa-pause-circle');
+
+})
+
+document.getElementById('previous').addEventListener('click', ()=>{
+  if(songIndex<=0){
+      songIndex = 0
+  }
+  else{
+      songIndex -= 1;
+  }
+  audioElement.src = `songs/${songIndex+1}.mp3`;
+  masterSongName.innerText = songs[songIndex].songName;
+  audioElement.currentTime = 0;
+  audioElement.play();
+  masterPlay.classList.remove('fa-play-circle');
+  masterPlay.classList.add('fa-pause-circle');
+})
